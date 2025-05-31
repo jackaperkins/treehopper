044aa78f30545a72e20173bb7333f1ff34954b8e
jackaperkins <jackaperkins@posteo.net>
2025-05-31 12:55:29 +0200

    respond with End when incoming query is out of range

--- src/node.rs
+++ src/node.rs
@@ -1,7 +1,7 @@
 #[derive(PartialEq, Debug)]
 pub enum NodeMessage {
     HasQuery { location: usize, value: u32 },
-    HasResponse(bool),
+    HasResponse{ location: usize, has: bool},
     End,
 }
 
@@ -27,16 +27,16 @@ impl<'a> NodeState<'a> {
     pub fn receive(&mut self, message: NodeMessage) -> NodeMessage {
         match message {
             NodeMessage::HasQuery { location, value } => match self.data.get(location) {
-                None => NodeMessage::HasResponse(false),
+                None => NodeMessage::End,
                 Some(val) => {
                     if *val == value {
-                        NodeMessage::HasResponse(true)
+                        NodeMessage::HasResponse{location, has: true}
                     } else {
-                        NodeMessage::HasResponse(false)
+                        NodeMessage::HasResponse{location, has: false}
                     }
                 }
             },
-            NodeMessage::HasResponse(has_it) => NodeMessage::End,
+            NodeMessage::HasResponse{location, has} => NodeMessage::End,
             NodeMessage::End => NodeMessage::End,
         }
     }
@@ -52,11 +52,18 @@ fn basic_has_query() {
         location: 0,
         value: 1,
     });
-    assert_eq!(response1, NodeMessage::HasResponse(true));
+    assert_eq!(response1, NodeMessage::HasResponse{location: 0, has:true});
 
     let response2 = node.receive(NodeMessage::HasQuery {
         location: 1,
         value: 1,
     });
-    assert_eq!(response2, NodeMessage::HasResponse(false));
+    assert_eq!(response2, NodeMessage::HasResponse{location: 1, has:false});
+
+    let response3 = node.receive(NodeMessage::HasQuery {
+        location: 9,
+        value: 1,
+    });
+    // expect end when either side runs out of elements
+    assert_eq!(response3, NodeMessage::End);
 }
