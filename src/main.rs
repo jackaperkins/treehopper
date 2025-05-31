777e05729be9982bff406fe4792602dbc0f8776d
jackaperkins <jackaperkins@posteo.net>
2025-05-31 14:27:31 +0200

    add tests for arrays of odd lengths, count round trip iterations of the protocol to prove we give up early

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,24 @@ fn main() {
     println!("Hello, world!");
 }
 
+
+/// check common elements position-wise between two arrays, wrapped in NodeStates. Return a counter of how many iterations it took
+/// 
+/// For example `[1,2,3]` and `[1,2,3]` have common elements `[1,2,3]`
+/// 
+/// But `[1,2,3]` and `[3,2,1]` only have `[2]` in common
+/// 
+/// This is only metaphorically related to walking a tree, this code serves no useful purpose now
 #[allow(unused)]
-fn protocol(node1: &mut NodeState, node2: &mut NodeState) {
+fn protocol(node1: &mut NodeState, node2: &mut NodeState) -> usize {
     let mut message = node1.start();
+    let mut counter = 0;
     while message != NodeMessage::End {
+        counter += 1;
         let response = node2.receive(message);
         message = node1.receive(response);
     }
+    counter
 }
 
 #[test]
@@ -38,4 +49,36 @@ fn basic_protocol_partial() {
 
     assert_eq!(node1.common, vec![9]);
     assert_eq!(node2.common, vec![9]);
+}
+
+#[test]
+fn protocol_odd_lengths() {
+    let data = vec![7, 9, 10, 11, 12, 13, 14];
+    let data2 = vec![8, 9];
+    let mut node1 = NodeState::new(&data);
+    let mut node2 = NodeState::new(&data2);
+
+    let iterations = protocol(&mut node1, &mut node2);
+
+    assert_eq!(node1.common, vec![9]);
+    assert_eq!(node2.common, vec![9]);
+
+    // prove that we dont spin through elements once the other side hung up
+    assert_eq!(iterations, 3);
+}
+
+#[test]
+fn protocol_odd_lengths_reversed() {
+    let data = vec![8, 9];
+    let data2 = vec![7, 9, 10, 11, 12, 13, 14];
+    let mut node1 = NodeState::new(&data);
+    let mut node2 = NodeState::new(&data2);
+
+    let iterations = protocol(&mut node1, &mut node2);
+
+    assert_eq!(node1.common, vec![9]);
+    assert_eq!(node2.common, vec![9]);
+
+    // initiator hangs up quicker because it's shorter, so only 2 loops
+    assert_eq!(iterations, 2);
 }
